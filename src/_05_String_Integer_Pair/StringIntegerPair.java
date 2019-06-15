e073bfc990996cdd2b29b86e9976a0e6a486d56f
League Student <league@iMac-47.attlocal.net>
2019-06-14 18:31:44 -0700

    String-Integer-Pair :sunglasses:

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -2,11 +2,13 @@ package _05_String_Integer_Pair;
 
 public class StringIntegerPair {
 	//1. create a private array of Strings called keys. Don't initialize it.
-
+	private String[] keys;
 	//2. create a private array of integers called values.
-	
+	private int values[];
 	StringIntegerPair(){
 		//3. initialize both member arrays to a length of 0
+		keys = new String[0];
+		values = new int[0];
 	}
 	
 	// 4. Complete the steps in the put method
@@ -15,46 +17,84 @@ public class StringIntegerPair {
 		//   the passed in String, set the value at that location to the
 		//   passed in value and return from the method.
 		
+		for (int i = 0; i< keys.length; i++) {
+			
+			if(keys[i] != null && 
+			(keys[i].equals(key))) {
+				values[i] = value;
+				return;
+			}
+		}
 		//B. create a String array that is one element longer than the keys
-		
+			String[] stringarray1 = new String[keys.length+1];
 		//C. create an integer array that is one element longer than values
-		
+			int[] integersarray1 = new int[values.length+1];
 		//D. set the last element of the new String array to the passed in key
-		
+			stringarray1[stringarray1.length-1] = key;
 		//E. set the last element of the new int array to the passed in value
-		
+			integersarray1[integersarray1.length-1] = value;
 		//F. iterate through the keys and values and copy the elements to the new arrays
-		
+		for(int i = 0 ; i < keys.length; i++) {
+			stringarray1[i] = keys[i];
+		}
+		for(int x = 0; x < values.length; x++) {
+			integersarray1[x] = values[x];
+		}
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
+		keys = stringarray1; 
+		values = integersarray1;
+		
+	//	System.out.println(keys.length);
+		for(String eachkey : keys) {
+			System.out.println(eachkey);
+			
+		}
+		
+	//	System.out.println(values.length);
+		
+		
 	}
 	
 	//5. Complete the method so it returns the value located at the passed in key.
 	//   If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+		for(int i = 0; i < keys.length; i++) {
+			if(keys[i].equals(key)) {
+				return i;
+			}
+		}
+		return Integer.MIN_VALUE;
 	}
 	
 	//6. Complete the containsKey method so that it returns true if the
 	//   passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
-		
+		for(int i = 0; i < keys.length; i++) {
+			if(keys[i].equals(key)) {
+				return true;
+			}
+			}
 		return false;
 	}
 	
 	//7. Complete the containsValue method so that it returns true if the
 	//   passed in value is contained in the values array
 	public boolean containsValue(int value) {
-		
+		for(int x = 0; x < values.length; x++) {
+			if(values[x] == value) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+		return keys;
 	}
 	
 	//9. Complete the getValues so it returns the values as an array
 	public int[] getValues() {
-		return null;
+		return values;
 	}
 }
